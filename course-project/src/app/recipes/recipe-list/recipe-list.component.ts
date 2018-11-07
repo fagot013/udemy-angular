d24e1c175d65085b0696448369f5b2f1ca984657
c-alexmala <gpl3App5>
2018-11-07 21:54:18 +0200

    define test recipe in array; define recipe template and use it as test in recipe list

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 
 @Component ({
@@ -7,7 +8,9 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://get.pxhere.com/photo/restaurant-dish-food-menu-recipe-fast-food-meat-bread-hamburger-sandwich-beer-cook-cheeseburger-gastronomy-slider-burgers-dining-room-appetizer-french-fries-cholesterol-junk-food-finger-food-patty-veggie-burger-breakfast-sandwich-american-food-buffalo-burger-1374894.jpg')
+  ];
 
   constructor() {
 
