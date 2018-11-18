3820aa94f283be787e2ef6a8ae3eb1bed287e333
c-alexmala <gpl3App5>
2018-11-18 19:10:46 +0200

    directives: first basic attribute directive

---
+++ directives-start/src/app/basic-highlight/basic-hightlight.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighLight]'
+})
+export class BasicHightlightDirective implements OnInit {
+
+  constructor(private elementRef: ElementRef) {
+
+  }
+
+  ngOnInit(): void {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+}
