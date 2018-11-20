5d3b1f3421623307052aa96388cc2e1958b7ed7a
c-alexmala <gpl3App5>
2018-11-20 17:47:18 +0200

    course project: dropDown directive - course solution

--- course-project/src/app/shared/dropdown.directive.ts
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -1,23 +1,13 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  isOpen = false;
-
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {
-  }
+  @HostBinding('class.open') isOpen = false;
 
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
-    if (this.isOpen) {
-      this.renderer.addClass(this.elRef.nativeElement, 'open');
-    } else {
-      this.renderer.removeClass(this.elRef.nativeElement, 'open');
-    }
-
   }
-
 
 }
