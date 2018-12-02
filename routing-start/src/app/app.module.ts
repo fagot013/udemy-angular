340e6b0f02bc7b1b053c089d00eff6d3388dc851
c-alexmala <gpl3App5>
2018-12-02 13:23:58 +0200

    routing: nested routing

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,11 +15,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'servers', component: ServersComponent},
-  {path: 'servers/:id/edit', component: EditServerComponent},
-  {path: 'servers/:id', component: ServerComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'users/:id/:name', component: UserComponent}
+  {path: 'servers', component: ServersComponent, children: [
+      {path: ':id/edit', component: EditServerComponent},
+      {path: ':id', component: ServerComponent},
+    ]},
+  {path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent}
+    ]}
+
 ];
 
 @NgModule({
