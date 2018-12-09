17c43ec0af7307567f92ffb5460291d6ca5a6d3f
c-alexmala <gpl3App5>
2018-12-09 13:05:50 +0200

    observables: my custom observable

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// noinspection TsLint
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+// import 'rxjs/Rx';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +15,37 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    // const myNumbers: Observable<number> = interval(1000);
+    // myNumbers.subscribe(
+    //   (num: number) => {
+    //     console.log(num);
+    //   }
+    // );
+    const myObservable: Observable<string> = Observable.create( (observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.complete();
+        observer.error(' this does not work');
+      }, 5000);
+      setTimeout(() => {
+        observer.next('first package');
+      }, 6000);
+    });
 
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('completed');
+      }
+    );
 }
