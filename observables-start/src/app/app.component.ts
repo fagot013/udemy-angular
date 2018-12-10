f692a6ebb58a9b847e9f6af9c4c1d94a0e9b1660
c-alexmala <gpl3App5>
2018-12-10 20:08:37 +0200

    forms-template driven: initial setup

--- observables-start/src/app/app.component.ts
+++ observables-start/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { UsersService } from './users.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   user1Activated = false;
   user2Activated = false;
 
