029fdd3d73af789c36f22c0021979e3ffaa2d5d5
c-alexmala <gpl3App5>
2018-12-17 20:36:01 +0200

    forms-reactive: reactive setup

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ export class AppComponent implements OnInit {
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required,
           this.forbiddenUserName.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email],
+          this.forbiddenEmail)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -40,4 +42,16 @@ export class AppComponent implements OnInit {
     return null;
   }
 
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>(((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 2500);
+    }));
+    return promise;
+  }
 }
