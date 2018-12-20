cb25d3e8857da51998410cffcd290f91300b395c
c-alexmala <gpl3App5>
2018-12-20 10:35:58 +0200

    forms-reactive: assignment - my solution

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  projectForm: FormGroup;
+  statuses = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required], [this.validProjectName]),
+      'email': new FormControl(null, [Validators.email, Validators.required]),
+      'status': new FormControl(null, Validators.required)
+    });
+  }
+
+  validProjectName(control: FormControl): Observable<any> | Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      if (control.value === 'Test') {
+        resolve({'invalidProjectName': true});
+      } else {
+        resolve(null);
+      }
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
+
 }
