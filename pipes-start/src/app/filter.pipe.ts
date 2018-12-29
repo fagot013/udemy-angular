c1181178cb1a58714b7753a684ecf17f8a6314f6
c-alexmala <gpl3App5>
2018-12-29 14:23:48 +0200

    pipes: custom inpure pipe 'filter'

---
+++ pipes-start/src/app/filter.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { v } from '@angular/core/src/render3';
+
+@Pipe({
+  name: 'filter',
+  pure: false
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterString: string, propertyName: string): any {
+    if (value.lenght > 0 || filterString === '' ) {
+      return value;
+    }
+    const resultArray = [];
+    for (const item of value) {
+      if (item[propertyName] === filterString) {
+        resultArray.push(item);
+      }
+    }
+    return resultArray;
+  }
+}
