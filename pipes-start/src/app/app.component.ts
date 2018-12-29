2acf1cff62356d8002165c12a7c89efd9bbd3d68
c-alexmala <gpl3App5>
2018-12-29 14:30:10 +0200

    pipes: async pipe

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -34,6 +34,12 @@ export class AppComponent {
   ];
   filterStatus = '';
 
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
