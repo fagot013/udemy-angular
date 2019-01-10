61f70ce2e8a8c5f8393b64a2b5056558da86536c
c-alexmala <gpl3App5>
2019-01-10 19:43:12 +0200

    course project: build shared module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -18,6 +17,7 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -25,7 +25,6 @@ import { RecipesModule } from './recipes/recipes.module';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    DropdownDirective,
     SignupComponent,
     SigninComponent
   ],
@@ -34,7 +33,8 @@ import { RecipesModule } from './recipes/recipes.module';
     FormsModule,
     HttpModule,
     RecipesModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
     ShoppingListService,
