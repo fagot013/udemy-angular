b1f75ab80f8bdb90917fb837983eace90f12fd72
c-alexmala <gpl3App5>
2019-01-11 20:21:45 +0200

    course project: mode providers to core module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,16 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { RecipeResolver } from './recipes/recipe-resolver.service';
 import { HttpModule } from '@angular/http';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -20,17 +15,12 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     HttpModule,
+    AuthModule,
     ShoppingListModule,
     SharedModule,
     CoreModule
   ],
   providers: [
-    ShoppingListService,
-    RecipeService,
-    RecipeResolver,
-    DataStorageService,
-    AuthService,
-    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
--- course-project/src/app/core/core.module.ts
+++ course-project/src/app/core/core.module.ts
@@ -3,6 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { RecipeResolver } from '../recipes/recipe-resolver.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,15 @@ import { AppRoutingModule } from '../app-routing.module';
   exports: [
     HeaderComponent,
     AppRoutingModule
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    RecipeResolver,
+    DataStorageService,
+    AuthService
   ]
+
 })
 export class CoreModule {
 
