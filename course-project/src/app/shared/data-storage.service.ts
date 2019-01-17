3f0e96f752d0a06da89344cd2dd8943a9d48f064
c-alexmala <gpl3App5>
2019-01-17 12:04:33 +0200

    course project: use httpclient module - set headers and params

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -4,7 +4,7 @@ import { Recipe } from '../recipes/recipe.model';
 // noinspection TsLint
 import 'rxjs/Rx';
 import { AuthService } from '../auth/auth.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 // import { Observable } from 'rxjs';
 
 @Injectable()
@@ -16,17 +16,25 @@ export class DataStorageService {
 
   saveRecipes()  {
     const token = this.authService.getToken();
-    return this.httpClient.put(this.fireBaseUrl + '?auth=' + token, this.recipeService.getRecipes(), {
-      observe: 'body'
+    // return this.httpClient.put(this.fireBaseUrl, this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   // headers: new HttpHeaders().set('header1', 'value1'),
+    //   params: new HttpParams().set('auth', token)
+    // });
+    const req = new HttpRequest('PUT', this.fireBaseUrl, this.recipeService.getRecipes(), {
+      reportProgress: true,
+      params: new HttpParams().set('auth', token)
     });
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
     const token = this.authService.getToken();
     // return this.httpClient.get<Recipe[]>(this.fireBaseUrl + '?auth=' + token)
-    return this.httpClient.get<Recipe[]>(this.fireBaseUrl + '?auth=' + token, {
+    return this.httpClient.get<Recipe[]>(this.fireBaseUrl, {
       observe: 'body',
-      responseType: 'json'
+      responseType: 'json',
+      params: new HttpParams().set('auth', token)
     })
       .map(
         (recipes) => {
@@ -37,7 +45,6 @@ export class DataStorageService {
             }
           }
           return recipes;
-
         })
       .subscribe(
       (recipes: Recipe[]) => {
